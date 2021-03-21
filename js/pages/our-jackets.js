7deded835d47274da813e26bc994d52d0f5cbbeb
Marie Lowzow Morland <marie@morland.no>
2021-03-21 22:03:57 +0100

    -replaced last label + hidden checkbox trick with actual button

--- js/pages/our-jackets.js
+++ js/pages/our-jackets.js
@@ -10,3 +10,9 @@ const jacketContainger = document.querySelector(".jacket-container");
 products.forEach(jacket =>
   jacketContainger.insertAdjacentHTML("afterbegin", jacketBox(jacket))
 )
+
+const toggleMenu = (event) => {
+  document.getElementById("filter-menu-wrapper").classList.toggle("visible");
+} 
+
+document.getElementById("toggle-menu").addEventListener("click", toggleMenu);
